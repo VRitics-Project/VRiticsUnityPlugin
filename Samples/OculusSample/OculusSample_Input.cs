79d3b4d392298daf62ca1b86f665a306b5399a54
ttopczewski <toptom92@gmail.com>
2023-09-18 23:38:37 -0400

    Updated package

--- Samples/OculusSample/OculusSample_Input.cs
+++ Samples/OculusSample/OculusSample_Input.cs
@@ -5,7 +5,7 @@ public class OculusSample_Input : MonoBehaviour
 {
     private void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
         {
             VRiticsSetup.Input();
         }
